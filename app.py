af74c6c8a5d4dbc163fd22ba65489a858d7bd718
unknown <umar.ovie@gmail.com>
2022-12-09 00:54:45 +0700

    belajar

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+from process import preparation, generate_response
 from flask import Flask, render_template, request, url_for
+
+
+preparation()
 
 app = Flask(__name__)
 
@@ -17,6 +21,15 @@ def budaya():
 def wisata():
     return render_template("wisata.html")
 
+@app.route('/chatbot')
+def chatbot():
+    return render_template('chatbot.html')
+
+@app.route("/get")
+def get_bot_response():
+    user_input = str(request.args.get('msg'))
+    result = generate_response(user_input)
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
